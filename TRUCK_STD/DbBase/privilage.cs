491b13873a8721d49d70513a5ed9096fc4323461
RinRINRIRNRINRRIN <karinking9078loop@gmail.com>
2024-12-27 11:11:23 +0700

    กำหนด property privilage และแก้ไข query string

--- TRUCK_STD/DbBase/privilage.cs
+++ TRUCK_STD/DbBase/privilage.cs
@@ -14,6 +14,28 @@ namespace TRUCK_STD.DbBase
 
 
         public static string ERR { get; set; }
+        public static string user_id { get; set; }
+        public static string menu_id { get; set; }
+        public static string pr_add { get; set; }
+        public static string pr_edit { get; set; }
+        public static string pr_del { get; set; }
+
+        public static bool SelectPrivilage(string _user)
+        {
+            try
+            {
+                sql = $"SELECT * FROM privilage WHERE user_id = '{_user}'";
+                da = new MySqlDataAdapter(sql, con);
+                tb = new DataTable();
+                da.Fill(tb);
+            }
+            catch (System.Exception ex)
+            {
+                ERR = ex.Message;
+                return false;
+            }
+            return true;
+        }
 
         public static bool Select(privilageModels privilage)
         {
@@ -36,7 +58,7 @@ namespace TRUCK_STD.DbBase
         }
 
 
-        public static bool Insert(privilageModels privilage)
+        public static bool Insert()
         {
             try
             {
@@ -44,11 +66,11 @@ namespace TRUCK_STD.DbBase
                     "VALUES(@user_id,@menu_id,@pr_add,@pr_del,@pr_edit)";
 
                 cmd = new MySqlCommand(sql, con);
-                cmd.Parameters.Add(new MySqlParameter("@user_id", privilage.user_id));
-                cmd.Parameters.Add(new MySqlParameter("@menu_id", privilage.menu_id));
-                cmd.Parameters.Add(new MySqlParameter("@pr_add", privilage.add));
-                cmd.Parameters.Add(new MySqlParameter("@pr_del", privilage.del));
-                cmd.Parameters.Add(new MySqlParameter("@pr_edit", privilage.edit));
+                cmd.Parameters.Add(new MySqlParameter("@user_id", user_id));
+                cmd.Parameters.Add(new MySqlParameter("@menu_id", menu_id));
+                cmd.Parameters.Add(new MySqlParameter("@pr_add", pr_add));
+                cmd.Parameters.Add(new MySqlParameter("@pr_del", pr_del));
+                cmd.Parameters.Add(new MySqlParameter("@pr_edit", pr_edit));
                 cmd.ExecuteNonQuery();
             }
             catch (System.Exception ex)
@@ -74,11 +96,11 @@ namespace TRUCK_STD.DbBase
         }
 
 
-        public static bool Delete(privilageModels privilageModels)
+        public static bool Delete(string _user)
         {
             try
             {
-                sql = $"DELETE FROM privilage WHERE user_id = '{privilageModels.user_id}'";
+                sql = $"DELETE FROM privilage WHERE user_id = '{_user}'";
                 cmd = new MySqlCommand(sql, con);
                 cmd.ExecuteNonQuery();
             }
