ad1168a740eebaf32d74df4a2bb635f9ad95c90b
RinRINRIRNRINRRIN <karinking9078loop@gmail.com>
2024-12-28 13:43:18 +0700

    แก้ไขบรรทัด

--- TRUCK_STD/Models/menuModels.cs
+++ TRUCK_STD/Models/menuModels.cs
@@ -5,6 +5,8 @@ namespace TRUCK_STD.Models
 {
     internal class menuModels
     {
+
+
         public static Dictionary<string, Tuple<string, string, string>> menuList = new Dictionary<string, Tuple<string, string, string>>();
 
     }
