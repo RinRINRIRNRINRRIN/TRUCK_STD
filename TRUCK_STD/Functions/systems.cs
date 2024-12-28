7bb5a654e4b60921d5c53f12c1601a6979598cec
RinRINRIRNRINRRIN <karinking9078loop@gmail.com>
2024-12-28 13:25:44 +0700

    เพิ่ม prop สำหรับการแก้ไข

---
+++ TRUCK_STD/Functions/systems.cs
@@ -0,0 +1,55 @@
+﻿namespace TRUCK_STD.Functions
+{
+    internal class systems
+    {
+        public static string KeyId
+        {
+            get { return registy.system.keyID; }
+        }
+
+
+        public static string PublicKey
+        {
+            get { return registy.system.publicKey; }
+        }
+
+        public static string BussinessType
+        {
+            get { return registy.system.bussinessType; }
+        }
+
+        public static string Date
+        {
+            get { return registy.system.date; }
+        }
+
+        public static string DateExpire
+        {
+            get { return registy.system.dateExpire; }
+        }
+
+        /// <summary>
+        /// ประเภทการใช้งานโปรแกรม TRUE = ใช้งานจริง , FALSE = ใช้งานทดสอบ
+        /// </summary>
+        public static string ProgramType
+        {
+            get { return registy.system.programType; }
+        }
+
+        public static string StationName
+        {
+            get { return registy.system.stationName; }
+        }
+
+
+        /// <summary>
+        /// ประเภทของการใช้โปรแกรม (สถานีชั่ง , จุดลงทะเบียน)
+        /// </summary>
+        public static string StationType
+        {
+            get { return registy.system.stationType; }
+        }
+
+
+    }
+}
